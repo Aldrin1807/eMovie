b37994f72bb44ff231d4117489bc0e6f3fe0b758
Aldrin1807 <ai58914@ubt-uni.net>
2023-06-23 20:30:36 +0200

    added form and logic to add a new actor. Needs some changes

--- eMovie/Controllers/ActorsController.cs
+++ eMovie/Controllers/ActorsController.cs
@@ -1,6 +1,8 @@
 ﻿using eMovie.Data.Services;
+using eMovie.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace eMovie.Controllers
 {
@@ -17,6 +19,23 @@ namespace eMovie.Controllers
         {
             var data = await _service.GetAllActors();
             return View(data);
+        }
+
+        //GET: Actors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("FullName,ProfilePictureURL,Bio")]Actor actor)
+        {
+           if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            _service.AddActor(actor);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
